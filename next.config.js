d16eaae501a80dcf447be1afddd2196eabb24abc
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-01-04 13:20:55 +0530

    first

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            'oaidalleapiprodscus.blob.core.windows.net',
+            'another-domain.com',
+            // Add more domains as needed
+        ],
+    },
+}
 
 module.exports = nextConfig
---
+++ src/lib/firebase.ts
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "aideation-5f1d6.firebaseapp.com",
+  projectId: "aideation-5f1d6",
+  storageBucket: "aideation-5f1d6.appspot.com",
+  messagingSenderId: "429368835237",
+  appId: "1:429368835237:web:0fbc1b4b489745a46c214c",
+  measurementId: "G-CWSRK47SF4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
