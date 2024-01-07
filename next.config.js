dffb0edc805e11c92fdf2e7dc8187357711a34f5
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-01-07 19:47:14 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        reactStrictMode: true,
         domains: [
             'oaidalleapiprodscus.blob.core.windows.net',
             'another-domain.com',
+            "randomuser.me",
+            "firebasestorage.googleapis.com",
             // Add more domains as needed
         ],
         remotePatterns: [
@@ -13,6 +16,14 @@ const nextConfig = {
                 pathname: '**',
             },
         ],
+        webp: {
+            preset: "default",
+            quality: 100,
+        },
+        i18n: {
+            locales: ["en", "it"],
+            defaultLocale: "en",
+        },
     },
 }
 
